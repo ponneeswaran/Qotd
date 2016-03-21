bd6c7d88eac12d2a303faa4cb9e4e06060b30d8b
anandh <anandh@Anandh>
2016-03-20 21:28:16 -0700

    db connection

---
+++ src/main/java/com/awqotd/dao/DBConnection.java
@@ -0,0 +1,34 @@
+package com.awqotd.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection 
+{
+	public static Connection getConnection()
+	{
+		Connection connection = null;
+		try 
+		{
+			Class.forName("com.mysql.jdbc.Driver");
+			connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/qotd","adminWrhucm3","qvC2LFh6la5a");
+		}
+		catch (ClassNotFoundException e) 
+		{
+			e.printStackTrace();
+		}
+		catch (SQLException e) 
+		{
+			e.printStackTrace();
+		}
+		return connection;
+	}
+	public static void main(String[] args) throws SQLException 
+	{
+		Connection conn = DBConnection.getConnection();
+		System.out.println(conn.isClosed());
+		conn.close();
+		System.out.println(conn.isClosed());
+	}
+}
