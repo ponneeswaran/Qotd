b279fd8e8458fabcf2b63b1804e4dbae98b888a7
Ajith <23ajith7@gmail.com>
2016-03-25 14:43:00 -0700

    Included attributes for DAG Node

--- src/main/java/com/awqotd/vo/Node.java
+++ src/main/java/com/awqotd/vo/Node.java
@@ -8,5 +8,34 @@ package com.awqotd.vo;
  *
  */
 public class Node {
-
+  private String tagName;
+	private String url;
+	private int id;
+	
+	public String getTagName()
+	{
+		return this.tagName;
+	}
+	public void setTagName(String tagName)
+	{
+		this.tagName = tagName;
+	}
+	
+	public String getUrl()
+	{
+		return this.url;
+	}
+	public void setUrl(String url)
+	{
+		this.url = url;
+	}
+	
+	public int getId()
+	{
+		return this.id;
+	}
+	public void setId(int id)
+	{
+		this.id = id;
+	}
 }
