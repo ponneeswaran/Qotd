c6f6a96f62cd55d75f4c1ec717786e828e9557cf
Ajith <23ajith7@gmail.com>
2016-03-25 14:42:26 -0700

    Included attributes for DAG Edge

--- src/main/java/com/awqotd/vo/Edge.java
+++ src/main/java/com/awqotd/vo/Edge.java
@@ -8,5 +8,34 @@ package com.awqotd.vo;
  *
  */
 public class Edge {
-
+  private Node source;
+	private Node target;
+	private double weight;
+	
+	public Node getSource()
+	{
+		return this.source;
+	}
+	public void setSource(Node source)
+	{
+		this.source = source;
+	}
+	
+	public Node getTarget()
+	{
+		return this.target;
+	}
+	public void setTarget(Node target)
+	{
+		this.target = target;
+	}
+	
+	public double getWeight()
+	{
+		return this.weight;
+	}
+	public void setWeight(double weight)
+	{
+		this.weight = weight;
+	}
 }
